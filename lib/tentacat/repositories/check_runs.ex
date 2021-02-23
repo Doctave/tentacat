83c9f0fb0d1c367ada8da0a75f18d372abf61f32
Niklas Begley <niklas.begley@gmail.com>
2021-02-23 19:03:01 +0200

    Fix bad variable names

--- lib/tentacat/repositories/check_runs.ex
+++ lib/tentacat/repositories/check_runs.ex
@@ -10,19 +10,19 @@ defmodule Tentacat.Repositories.CheckRun do
     patch("repos/#{owner}/#{repo}/check-runs/#{check_run_id}", client, body)
   end
 
-  def find(client, owner, repo, ref, body) do
+  def find(client, owner, repo, check_run_id, body) do
     get("repos/#{owner}/#{repo}/check-runs/#{check_run_id}", client)
   end
 
   def list_for_ref(client, owner, repo, ref, body) do
-    get("repos/#{owner}/#{repo}/commits/#{check_run_id}/check-runs", client)
+    get("repos/#{owner}/#{repo}/commits/#{ref}/check-runs", client)
   end
 
   def list_for_suite(client, owner, repo, check_suite_id) do
     get("repos/#{owner}/#{repo}/check-suites/#{check_suite_id}/check-runs", client)
   end
 
-  def list_annotations(client, owner, repo, ref, body) do
-    get("repos/#{owner}/#{repo}/commits/#{check_run_id}/check-runs/annotations", client)
+  def list_annotations(client, owner, repo, check_run_id, body) do
+    get("repos/#{owner}/#{repo}/check-runs/#{check_run_id}/annotations", client)
   end
 end
